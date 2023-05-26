a0989fd1af174705f83c99470c7936059c629bb2
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-05-26 15:32:35 -0300

    🧪 test: refactor mocks and ensure replace method returns correct value

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,36 +1,45 @@
 const assert = require('assert');
-const Filter = require('./index.js')
+const InappropriateWordsFilter = require('../index.js')
 
-const filter = new Filter()
+const inappropriateWords = new InappropriateWordsFilter()
 
+const INAPROPRIATE_PHRASE = 'Neste texto temos algumas palavras que serão filtradas, merda é uma palavra inapropriada, neste caso, o filtro deve retornar true'
 describe('Filter()', () => {
     describe('hasBadWords method', () => { 
         it('should return true when match a inappropriate word', () => {
-            const text = 'Neste texto temos algumas palavras que serão filtradas, merda é uma palavra inapropriada, neste caso, o filtro deve retornar true'
-            const result = filter.hasBadWords(text)
+            const result = inappropriateWords.check(INAPROPRIATE_PHRASE)
             assert.strictEqual(result, true)
         });
         it('should return false when not match a inappropriate word', () => {
             const text = 'Neste texto não temos palavras inapropriadas, neste caso, o filtro deve retornar false'
-            const result = filter.hasBadWords(text)
+            const result = inappropriateWords.check(text)
             assert.strictEqual(result, false)
         })
         it('should return false when text is empty', () => {
             const text = ''
-            const result = filter.hasBadWords(text)
+            const result = inappropriateWords.check(text)
             assert.strictEqual(result, false)
         })
         it('should return throw new Error when text is not a string', () => {
             const text = 123
-            assert.throws(() => filter.hasBadWords(text), Error('The argument must be a string'))
+            assert.throws(() => inappropriateWords.check(text), Error('The argument must be a string'))
         })
         
-     })
+    })
      describe('countBadWords', () => { 
         it('should return 1 when match a inappropriate word', () => {
-            const text = 'Neste texto temos algumas palavras que serão filtradas, merda é uma palavra inapropriada, neste caso, o filtro deve retornar true'
-            const result = filter.countBadWords(text)
+            const result = inappropriateWords.count(INAPROPRIATE_PHRASE)
             assert.strictEqual(result, 1)
         })
-      })
+    })
+    describe('replace', () => {
+        it('should return a text with asterisks when match a inappropriate word', () => {
+            const result = inappropriateWords.replace(INAPROPRIATE_PHRASE)
+            assert.strictEqual(result, 'Neste texto temos algumas palavras que serão filtradas, ***** é uma palavra inapropriada, neste caso, o filtro deve retornar true')
+        })
+        it('should return a text with 0 symbols when match a inappropriate word', () => {
+            const result = inappropriateWords.replace(INAPROPRIATE_PHRASE, '0')
+            assert.strictEqual(result, 'Neste texto temos algumas palavras que serão filtradas, 00000 é uma palavra inapropriada, neste caso, o filtro deve retornar true')
+        })
+    })
 })
