010eecae7e4b8887f79a498be56a5a142fadfab5
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-05-26 11:23:10 -0300

    📝 doc: add information about methods

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ class Filter {
     constructor() {
         this.list = list
     }
-
+    /**
+     * This method checks if the text contains bad words
+     * @param {string} text
+     * @returns {boolean}
+     */
     hasBadWords(text) {
         if(!text) return false
         if(typeof text !== 'string') throw new Error('The argument must be a string')
         text.toLocaleLowerCase()
         return this.list.some(word => text.includes(word))
     }
+    /**
+     *  This method counts how many bad words are in the text
+     * @param {string} text
+     * @returns {number} 
+     */
     countBadWords(text) {
         const words = text.split(' ')
         const badWords = words.filter(word => this.list.includes(word))
